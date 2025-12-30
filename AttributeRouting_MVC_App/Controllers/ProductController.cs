e252eb170e4ec03fcf4c4c3e95cdbc281bae6318
Md. Ibn Masud <sohan.cse.pust@gmail.com>
2025-12-30 22:03:10 +0600

    Add project files.

---
+++ AttributeRouting_MVC_App/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AttributeRouting_MVC_App.Models;
+
+namespace AttributeRouting_MVC_App.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product(){Id=1,Name="Laptop",Price=80000,Stock=10},
+            new Product(){Id=2,Name="Tablet",Price=45000,Stock=15},
+            new Product(){Id=3,Name="Smartphone",Price=30000,Stock=25},
+            new Product(){Id=4,Name="Smartwatch",Price=15000,Stock=30},
+        };
+        [Route("ShowProducts")]
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+        [Route("AddProduct")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("AddProduct")]
+        public IActionResult Create(Product product)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return View();
+            }
+            products.Add(product);
+            return RedirectToAction("Index");
+        }
+        [Route("ProductDetails/{id}")]
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+        [Route("EditProduct/{id}")]
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+        [Route("DeleteProduct/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+    }
+}
---
+++ AttributeRouting_MVC_App/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AttributeRouting_MVC_App.Models
+{
+    public class Product
+    {
+        [DisplayName("Product Id")]
+        [Required(ErrorMessage ="Product Id required")]
+        public int Id { get; set; }
+        [DisplayName("Product Name")]
+        [StringLength(50)]
+        [Required(ErrorMessage ="Product Name required")]
+        public string Name { get; set; }
+        [DisplayName("Product Price")]
+        [Required(ErrorMessage ="Product Price required")]
+        public decimal Price { get; set; }
+        [DisplayName("Units in Stock")]
+        [Required(ErrorMessage ="Units in Stock required")]
+        public int Stock { get; set; }
+    }
+}
